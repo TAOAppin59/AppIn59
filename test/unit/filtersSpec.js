1fd3e4489b00a3ad96ef3b8552b8047b1515220e
joetravis <joe.travis@adp.com>
2014-02-25 18:15:40 -0800

    adding styling functionality

--- test/unit/filtersSpec.js
+++ test/unit/filtersSpec.js
@@ -16,4 +16,50 @@ describe('filter', function () {
             expect(interpolateFilter('before %VERSION% after')).toEqual('before TEST_VER after');
         }));
     });
+
+    describe('goodThreshold', function () {
+        it('should be 90', inject(function(goodThreshold) {
+            expect(goodThreshold).toEqual(90);
+        }));
+    });
+
+    describe('cautionThreshold', function() {
+        it('should be 75', inject(function(cautionThreshold) {
+            expect(cautionThreshold).toEqual(75);
+        }));
+    });
+
+    describe('markerClass', function() {
+        it('should return green if above good threshold', inject(function(goodThreshold, markerClassFilter) {
+            expect(markerClassFilter(goodThreshold + 1)).toEqual('green');
+            expect(markerClassFilter(goodThreshold)).not.toEqual('green');
+        }));
+
+        it('should return yellow if above caution threshold', inject(function(cautionThreshold, markerClassFilter) {
+            expect(markerClassFilter(cautionThreshold + 1)).toEqual('yellow');
+            expect(markerClassFilter(cautionThreshold)).not.toEqual('yellow');
+        }));
+
+        it('should return red if below caution threshold', inject(function(cautionThreshold, markerClassFilter) {
+            expect(markerClassFilter(cautionThreshold)).toEqual('red');
+            expect(markerClassFilter(cautionThreshold + 1)).not.toEqual('red');
+        }));
+    });
+
+    describe('markerIcon', function() {
+        it('should return star if above good threshold', inject(function(goodThreshold, markerIconFilter) {
+            expect(markerIconFilter(goodThreshold + 1)).toEqual('img/star.png');
+            expect(markerIconFilter(goodThreshold)).not.toEqual('img/star.png');
+        }));
+
+        it('should return caution if above caution threshold', inject(function(cautionThreshold, markerIconFilter) {
+            expect(markerIconFilter(cautionThreshold + 1)).toEqual('img/caution.png');
+            expect(markerIconFilter(cautionThreshold)).not.toEqual('img/caution.png');
+        }));
+
+        it('should return skull if below caution threshold', inject(function(cautionThreshold, markerIconFilter) {
+            expect(markerIconFilter(cautionThreshold)).toEqual('img/skull.png');
+            expect(markerIconFilter(cautionThreshold + 1)).not.toEqual('img/skull.png');
+        }));
+    });
 });
